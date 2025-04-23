cd98c6b51a5e2287c55589529df9103f15ded241
GERRYP20 <gerryperezsanchez2@gmail.com>
2025-04-22 18:21:57 -0600

    Personajes
    -se añadieron 3 personajes nuevos al proyecto: Ana,Emma y Dario.

---
+++ source/personajes/p_dario.gd
@@ -0,0 +1,12 @@
+
+extends CharacterBody2D
+
+@onready var sprite = $PDario
+
+func _physics_process(delta):
+	var is_moving = velocity.length() > 0.1
+
+	if is_moving:
+		pass
+	else:
+			sprite.play("nada")
---
+++ source/personajes/p_emma.gd
@@ -0,0 +1,11 @@
+extends CharacterBody2D
+
+@onready var sprite = $PEmma
+
+func _physics_process(delta):
+	var is_moving = velocity.length() > 0.1
+
+	if is_moving:
+		pass
+	else:
+			sprite.play("nada")
