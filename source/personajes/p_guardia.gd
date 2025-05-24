cd69851018fa0d3c011a88361a49a1a354d77e58
GERRYP20 <gerryperezsanchez2@gmail.com>
2025-05-24 03:00:23 -0600

    guardias funcionales
    los guardias ya persiguen

--- source/personajes/p_guardia.gd
+++ source/personajes/p_guardia.gd
@@ -1,25 +1,39 @@
 extends CharacterBody2D
 
-# Referencia al Area2D del jugador detectado
 var area_jugador: Area2D = null
-
-# Velocidad de movimiento
 var vel := 150.0
+var posicion_ant: Vector2
+
+func _ready():
+	posicion_ant = position
 
 func _physics_process(delta):
 	if area_jugador and area_jugador.is_inside_tree():
 		var direccion = (area_jugador.global_position - global_position).normalized()
-		print("Pos enemigo:", global_position, "Pos jugador:", area_jugador.global_position, "Dir:", direccion)
 		
-		# Usa esta línea para que se mueva hacia el jugador:
-		velocity = direccion * vel
+		# Movimiento real hacia el jugador
+		position += direccion * vel * delta
 		
-		# Si ves que escapa hacia la izquierda, comenta la línea anterior y descomenta esta:
-		# velocity = -direccion * vel
+		_actualizar_animacion(direccion)
+		posicion_ant = position
 	else:
-		velocity = Vector2.ZERO
+		$AnimatedSprite2D.play("idle")
 
-	move_and_slide()
+func _actualizar_animacion(direccion: Vector2):
+	if direccion.length() < 0.01:
+		$AnimatedSprite2D.play("idle")
+		return
+
+	if abs(direccion.x) > abs(direccion.y):
+		if direccion.x > 0:
+			$AnimatedSprite2D.play("camina_derecha")
+		else:
+			$AnimatedSprite2D.play("camina_izquierda")
+	else:
+		if direccion.y > 0:
+			$AnimatedSprite2D.play("caminando_abajo")
+		else:
+			$AnimatedSprite2D.play("caminando_arriba")
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area.is_in_group("Jugador"):
