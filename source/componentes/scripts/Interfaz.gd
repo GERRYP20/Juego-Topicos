a8a1ef7bec930f02cc115aa4e72d5a6a9bf5a1ed
GERRYP20 <gerryperezsanchez2@gmail.com>
2025-05-25 02:46:04 -0600

    dialogos 2
    dialogos de dario totalmente funcionales

--- source/componentes/scripts/Interfaz.gd
+++ source/componentes/scripts/Interfaz.gd
@@ -1,6 +1,7 @@
 extends Control
 
 const icoDario = preload("res://resources/imagenes/dario.jpeg")
+var dialogo_index := 0  # ← índice actual del diálogo
 
 func _ready():
 	$CanvasLayer.hide()
@@ -12,6 +13,15 @@ func _activar(texto: String, imagen: Texture) -> void:
 
 func _process(delta):
 	if GDialogos.dario:
-		_activar(GDialogos.npc1.cap1.pd[0], icoDario)
+		if dialogo_index < GDialogos.npc1.cap1.pd.size():
+			_activar(GDialogos.npc1.cap1.pd[dialogo_index], icoDario)
+		else:
+			# Ya no hay más diálogos
+			GDialogos.dario = false
+			dialogo_index = 0
 	else:
 		$CanvasLayer.hide()
+
+func _on_texture_button_button_down() -> void:
+	if GDialogos.dario:
+		dialogo_index += 1  # Avanza al siguiente diálogo
--- source/componentes/scripts/dialogos.gd
+++ source/componentes/scripts/dialogos.gd
@@ -1,11 +1,19 @@
 extends Node
-var dario:bool=false;
 
-const npc1: Dictionary= {
-	'cap1':{
-		'pd':["¡HOLA PERRITO! MI NOMBRE ES DARIO"]
+var dario: bool = false
+
+const npc1: Dictionary = {
+	'cap1': {
+		'pd': [
+			"¡HOLA RAYAS! MI NOMBRE ES DARIO",
+			"¿QUÉ HACES POR AQUÍ?",
+			"SE VE QUE TIENES HAMBRE",
+			"DEBERÍAS IR A LA CAFETERIA",
+			"AHÍ PUEDE QUE ENCUENTRES COMIDA",
+			"¡PERO TEN CUIDADO CON LOS GUARDIAS!",
+			"NO LES GUSTAN LOS PERROS"
+		]
 	}
 }
-
 func _ready():
 	pass
